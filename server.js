138c9f267ec30f946e23c2b332d562fdb6540856
Carlos Sosa <carlossosa54321@gmail.com>
2022-08-01 18:05:31 -0700

    try again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ MongoClient.connect(dbConnectionString)
     })
  
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static('public')) 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
